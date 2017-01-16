19e02c4ca64e8068c61722b5568232664bb7bd9d
bfindlay <bfin5330@uni.sydney.edu.au>
2017-01-16 19:14:05 +1100

    added multidimentional array for grid component rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Cell from './Cell.jsx';
+import CellRow from './CellRow.jsx';
 class App extends Component {
 
   constructor(props){
@@ -11,25 +12,61 @@ class App extends Component {
   }
   generateCells(size){
     this.setState({width: size*12})
-    let cells = [];
+    let arr = [];
     for(let x = 0; x < size; x++){
-      for(let y = 0; y < size; y++){
-        cells.push({ x, y })
-      }
+        arr[x] = [];    
+        for(let y = 0; y < size; y++){ 
+            let c = new C(x, y);
+            arr[x][y] = c;    
+        }    
     }
-    this.setState({cells})
+    this.setState({cells: arr})
   }
+
   componentWillMount(){
-    this.generateCells(50);
+    this.generateCells(10);
   }
+
   render() {
     return (
       <div className='container'>
         <div style={{"width" : this.state.width}} className='cell-container'>
-            { this.state.cells.map( e => <Cell key={Math.random()} coords={{x: e.x, y:e.y}} />) }
+            { this.state.cells.map( (arr) => {
+                arr = arr.map( element => {
+                  return <Cell key={Math.random()} coords={element.location}/> 
+                });
+                return <CellRow cells={arr} />
+            })}
         </div>
       </div>
     )
+  }
+}
+
+class C {
+  constructor(x, y){
+    this.location = { 
+      x: x,
+      y: y
+    }
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+    this.child = null;
+    this.health = 1;
+  }
+  linkNode(node){
+    const { parent, left, right, child } = node;
+    this.parent = parent;
+    this.left = left;
+    this.right = right;
+    this.child = child;
+  }
+  setHealth(x){
+    this.health = x;
+  }
+  getNeighbours(){
+    return { parent: this.parent, left: this.left, right:this.right, child: this.child};
   }
 }
 
